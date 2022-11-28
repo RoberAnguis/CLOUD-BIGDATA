8ef9f2b3c3edbaa625ee60cd80f8ec7bd1eb1c79
Pablo1003 <55923015+Pablo1003@users.noreply.github.com>
2022-11-28 10:10:20 +0100

    Update proyecto.py
    t_max_avg_4.5

--- proyecto.py
+++ proyecto.py
@@ -20,35 +20,33 @@ df = spark.read.option("header", "true").csv("datosTmax.csv")
 
 #quitar los blancos más adelante
 
-#Avg por año, mes y modelo
-df.groupBy("Anio", "Mes", "Modelo").agg({'Dia1':'avg', 'Dia2':'avg', 'Dia3':'avg', 'Dia4':'avg', 'Dia5':'avg', 'Dia6':'avg', 'Dia7':'avg', 'Dia8':'avg', 'Dia9':'avg', 'Dia10':'avg', 'Dia11':'avg', 'Dia12':'avg', 'Dia13':'avg', 'Dia14':'avg', 'Dia15':'avg', 'Dia16':'avg', 'Dia17':'avg', 'Dia18':'avg', 'Dia19':'avg', 'Dia20':'avg', 'Dia21':'avg', 'Dia22':'avg', 'Dia23':'avg', 'Dia24':'avg', 'Dia25':'avg', 'Dia26':'avg', 'Dia27':'avg', 'Dia28':'avg', 'Dia29':'avg', 'Dia30':'avg', 'Dia31':'avg'}).show()
+#Avg por año
+df.groupBy("Anio").agg({'Dia1':'avg', 'Dia2':'avg', 'Dia3':'avg', 'Dia4':'avg', 'Dia5':'avg', 'Dia6':'avg', 'Dia7':'avg', 'Dia8':'avg', 'Dia9':'avg', 'Dia10':'avg', 'Dia11':'avg', 'Dia12':'avg', 'Dia13':'avg', 'Dia14':'avg', 'Dia15':'avg', 'Dia16':'avg', 'Dia17':'avg', 'Dia18':'avg', 'Dia19':'avg', 'Dia20':'avg', 'Dia21':'avg', 'Dia22':'avg', 'Dia23':'avg', 'Dia24':'avg', 'Dia25':'avg', 'Dia26':'avg', 'Dia27':'avg', 'Dia28':'avg', 'Dia29':'avg', 'Dia30':'avg', 'Dia31':'avg'}).show()
 
 
 
-def average(line): #para cada tupla año,mes,modelo calcula la media de temperatura
+def average(line): #para cada tupla año,mes calcula la media de temperatura
 	sum = 0
 	for i in range (3, 34):#no se si estan bien los rangos
 		sum += line[i]
-	return (line[0], line[1], line[2], sum/31)
+	return (line[0], sum/31)
 
 
 #Hacer el avg de cada día
-rddAux = df.rdd.map(average). collect() #rdd con tuplas año,mes,modelo,media de temperatura(en ese mes de ese año y con ese modelo)
+t_max_avg_4.5 = df.rdd.map(average).collect() #rdd con tuplas (año,media de temperatura)
 
 ''' Ejemplo:
 
-Año	Mes 	Modelo		Avg
+Año	 Avg
 
-2006     1        1.0            34
-2006     2        1.0            14
+2006     34
+2007     14
 .
 .
 .
-2006    1         2.0           23
+2020     23
 .
 .
 .
-2100    12        17.0          27
+2100     27
  '''
-
-#De aqui podemos sacar mas facilmente ya media por años o modelos, segun nos apetezca
